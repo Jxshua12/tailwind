044586716d5c0e864b1628985f84f2569eb60047
Dipo <joshuakaay1@gmail.com>
2026-09-15 17:48:11 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "rgb(244,67,54)",
+        button: "rgb(0,0,0)",
+      },
+    },
+  },
+  plugins: [],
+}
+
